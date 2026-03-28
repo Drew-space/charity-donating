61c44f43d4b7e6079007331c693abc3324d1bcd7
Drew <chukwukaenudeme@gmail.com>
2026-03-28 06:04:15 +0100

    feat: Added payment integration with paystack

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,88 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Button } from "./ui/button";
+
+import { Input } from "./ui/input";
+
+import z from "zod/v3";
+import { error } from "console";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { email } from "zod/v4-mini";
+import dynamic from "next/dynamic";
+
+const schema = z.object({
+  email: z.string().email(),
+  name: z.string(),
+  phone: z.string().min(10),
+  amount: z.number().min(100),
+});
+
+type formData = z.infer<typeof schema>;
+const PaystackButton = dynamic(
+  () => import("react-paystack").then((mod) => mod.PaystackButton),
+  { ssr: false },
+);
 
 const Hero = () => {
+  const [open, setOpen] = useState(false);
+
+  const publicKey = process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY || "";
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<formData>({
+    resolver: zodResolver(schema),
+  });
+
+  const email = watch("email");
+  const amount = watch("amount");
+
+  const componentProps = {
+    email,
+    amount: amount * 100,
+    publicKey,
+    currency: "NGN",
+
+    metadata: {
+      custom_fields: [
+        {
+          display_name: "Email",
+          variable_name: "email",
+          value: email,
+        },
+        {
+          display_name: "Amount",
+          variable_name: "amount",
+          value: amount,
+        },
+      ],
+    },
+
+    text: "Donate",
+    onSuccess: () => {
+      setOpen(false);
+      alert("Thank you for donating");
+    },
+    onClose: () => alert("Are you sure you want to close"),
+  };
+
   return (
     <div className="relative w-full h-100 md:h-132.5">
       <Image
@@ -34,9 +115,75 @@ const Hero = () => {
               villages in Nigeria by {new Date().getFullYear()}
             </p>
 
-            <button className="mt-6 bg-[#5aaa3a] text-white px-6 py-2 rounded-full text-sm md:text-base font-semibold ">
-              Donate now
-            </button>
+            <Dialog open={open} onOpenChange={setOpen}>
+              <form>
+                <DialogTrigger asChild>
+                  <button
+                    className="mt-6 bg-[#5aaa3a] text-white px-6 py-2 rounded-full text-sm md:text-base font-semibold "
+                    onClick={() => setOpen(true)}
+                  >
+                    Donate now
+                  </button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-sm">
+                  <DialogHeader>
+                    <DialogTitle>Donate Now</DialogTitle>
+                    <DialogDescription>
+                      Make changes to your profile here. Click save when
+                      you&apos;re done.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="flex flex-col gap-4">
+                    <div className="">
+                      <Input placeholder="Name" {...register("name")} />
+                      <p className="text-red-500 text-sm">
+                        {errors.name?.message}
+                      </p>
+                    </div>
+
+                    <div className="">
+                      <Input placeholder="Email" {...register("email")} />
+                      <p className="text-red-500 text-sm">
+                        {errors.email?.message}
+                      </p>
+                    </div>
+                    <div className="">
+                      <Input
+                        placeholder="Amount"
+                        {...register("amount", { valueAsNumber: true })}
+                      />
+                      <p className="text-red-500 text-sm">
+                        {errors.amount?.message}
+                      </p>
+                    </div>
+                    <div className="">
+                      <Input
+                        placeholder="Phone-Number"
+                        {...register("phone")}
+                      />
+                      <p className="text-red-500 text-sm">
+                        {errors.phone?.message}
+                      </p>
+                    </div>
+                  </div>
+
+                  <DialogFooter className="flex items-center">
+                    <DialogClose asChild>
+                      <Button variant="outline">Cancel</Button>
+                    </DialogClose>
+                    {email && amount !== undefined && (
+                      <div className="" onClick={() => setOpen(false)}>
+                        <PaystackButton
+                          {...componentProps}
+                          className="bg-[#5aaa3a] text-white rounded-md py-2 px-2"
+                          text="Donate"
+                        />
+                      </div>
+                    )}
+                  </DialogFooter>
+                </DialogContent>
+              </form>
+            </Dialog>
           </div>
         </div>
       </div>
